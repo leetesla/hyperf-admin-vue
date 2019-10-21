14596e9bb764e2310d0e55a3bf77dd6f1f3d34d2
penghaicheng <375488452@qq.com>
2019-10-21 16:41:37 +0800

    Hyperf-admin-vue init

---
+++ src/views/common/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mod-home">
+    <h3>项目介绍</h3>
+    <ul>
+      <li>hyperf-admin-vue基于vue、element-ui构建开发，实现<a href="" target="_blank">hyperf-admin</a>后台管理前端功能，提供一套更优的前端解决方案</li>
+      <li>演示地址：<a href="" target="_blank"></a> (账号密码：admin/admin)</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .mod-home {
+    line-height: 1.5;
+  }
+</style>
+

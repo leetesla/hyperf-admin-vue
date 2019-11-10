c5365cf918ba1e4b56ba1f9ee5d17f3610be6693
derek <2865900737@qq.com>
2019-11-10 21:52:09 +0800

    update home.vue

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -2,8 +2,10 @@
   <div class="mod-home">
     <h3>项目介绍</h3>
     <ul>
-      <li>hyperf-admin-vue基于vue、element-ui构建开发，实现<a href="" target="_blank">hyperf-admin</a>后台管理前端功能，提供一套更优的前端解决方案</li>
-      <li>演示地址：<a href="" target="_blank"></a> (账号密码：admin/admin)</li>
+      <li>Hyperf-admin前端基于vue、element-ui构建开发，实现<a href="#" target="_blank">Hyperf-admin</a>后台管理前端功能，提供一套更优的前端解决方案</li>
+      <li>后台Git地址：<a href="https://github.com/penghcheng/hyperf-admin" target="_blank">https://github.com/penghcheng/hyperf-admin</a></li>
+      <li>前台Git地址：<a href="https://github.com/penghcheng/hyperf-admin-vue" target="_blank">https://github.com/penghcheng/hyperf-admin-vue</a></li>
+      <li>如需关注项目最新动态，请Watch、Star项目，同时也是对项目最好的支持</li>
     </ul>
   </div>
 </template>
